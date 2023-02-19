0563f695b3a3a30b95f5e14ff2ff6db16367d349
Omer Baflah <aldonramos@gmail.com>
2023-02-20 02:11:17 +0300

    Add Scoped Service To Order Item Repository

--- AB12/Infrastructure/Components/OrderItemRepo.cs
+++ AB12/Infrastructure/Components/OrderItemRepo.cs
@@ -1,9 +1,11 @@
 ﻿using AB12.Domain.Base.Schema;
 using AB12.Domain.Persistence;
 using AB12.Infrastructure.Components.Common;
+using TanvirArjel.Extensions.Microsoft.DependencyInjection;
 
 namespace AB12.Infrastructure.Components
 {
+    [ScopedService]
     public class OrderItemRepo : BaseRepo<OrderItem>
     {
         private readonly AppDbContext _context;
