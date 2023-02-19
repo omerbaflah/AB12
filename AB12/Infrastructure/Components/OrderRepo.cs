f7045f44320f0eabba8e202efbaab93eb902e000
Omer Baflah <aldonramos@gmail.com>
2023-02-20 02:20:08 +0300

    Fixes Order Repository & Add Scoped Service

--- AB12/Infrastructure/Components/OrderRepo.cs
+++ AB12/Infrastructure/Components/OrderRepo.cs
@@ -1,13 +1,15 @@
 ﻿using AB12.Domain.Base.Schema;
 using AB12.Domain.Persistence;
 using AB12.Infrastructure.Components.Common;
+using TanvirArjel.Extensions.Microsoft.DependencyInjection;
 
 namespace AB12.Infrastructure.Components
 {
-    public class ProductItemRepo : BaseRepo<Order>
+    [ScopedService]
+    public class OrderRepo : BaseRepo<Order>
     {
         private readonly AppDbContext _context;
-        public ProductItemRepo(AppDbContext context) : base(context)
+        public OrderRepo(AppDbContext context) : base(context)
         {
             _context = context;
         }
