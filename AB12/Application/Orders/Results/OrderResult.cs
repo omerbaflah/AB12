1edf12396b5fc669b714268f6257c1bcace9eb41
Omer Baflah <aldonramos@gmail.com>
2023-02-20 02:46:56 +0300

    Add Order Result

---
+++ AB12/Application/Orders/Results/OrderResult.cs
@@ -0,0 +1,17 @@
+﻿using AB12.Services.Application;
+using AB12.Domain.Base.Schema;
+using AutoMapper;
+
+namespace AB12.Application.Products.Results
+{
+    public class OrderResult : IMapFrom<Order>
+    {
+        public string Id { get; set; }
+        public string ClientName { get; set; }
+        public string Status { get; set; }
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<Order, OrderResult>();
+        }
+    }
+}
